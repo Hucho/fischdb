d0a181c1f8bb0bcea866d5d3cb8eb3718939a45f
Hucho <matthias.abele@outlook.com>
2016-03-15 23:36:53 +0100

    Heute Nacht

--- server.js
+++ server.js
@@ -7,24 +7,60 @@ var Schema = mongoose.Schema;
 //Deleted
 
 //connect to db
-db = mongoose.createConnection('localhost','fische');
+var db = mongoose.createConnection('localhost','fische');
 db.on('error', console.error.bind(console,'connection error:'));
+db.on('connected',function(){
+	console.log("Connection to mongodb successfully established!");
+});
+
+//disconnect from mongodb when node stops
+
+process.on('SIGINT', function(){
+	mongoose.connection.close(function(){
+		console.log("Mongoose default connection terminated by app termination!");
+		process.exit(0);
+	});
+
+});
 
 //create a schema
-var userSchema = new Schema ({ name: String}, {collection: 'fische'});
+var userSchema = new Schema (
+	{ 
+		anzahl: Number,
+		art: String,
+		gewicht: Number,
+		strecken_id: Number,
+	}, 	{collection: 'kkr'});
 
 //model
 var User = db.model('User', userSchema);
 
+
+//alle Fische
 app.get('/fische', function(req,res){
 
 User.find({}, function(err,docs){
 	res.json(docs)
-	console.log(docs);
+	console.log("Alle Fische wurden versendet");
 
 });
 
 });
+
+//test gruppieren
+app.get('/gesamt', function(req,res){
+
+	User.find({}, function(err,docs){
+
+			docs.forEach(function(docs){
+
+				res.json(docs);
+			});
+
+	});
+
+});
+
 
 
 app.use(express.static(__dirname + "/public"));
